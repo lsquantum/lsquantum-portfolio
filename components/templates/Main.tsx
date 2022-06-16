c6d6b7ecc6236784d7517fa9a03edc263cf73892
lsquantum <ahmet_umut_serefoglu@hotmail.com>
2022-06-16 11:42:24 +0300

    eslintrc

--- components/templates/Main.tsx
+++ components/templates/Main.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-
+import Image from 'next/image';
 import { Background } from '../background/Background';
 import { Section } from '../layout/Section';
 import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
@@ -29,7 +29,7 @@ const Main = () => (
           <div className="w-full bg-gray-100 shadow-md rounded">
             <div className="p-4 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-12 border-b border-gray-300">
               <div className='w-full col-span-2'>
-                <img className="w-42 h-42 rounded-full mx-auto shadow-xl" src="/assets/images/profile-pic.png" alt="" width="384" height="512" />
+                <Image className="w-42 h-42 rounded-full mx-auto shadow-xl" src="/assets/images/profile-pic.png" alt="" width="384" height="512" />
               </div>
               <div className='w-auto ml-5 col-span-7'>
                 <div>
